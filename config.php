70a421b2c35b842c91acf9a2f436b92f7aa56c72
BenardM <maxime.benardb2b@gmail.com>
2017-01-29 21:08:03 +0100

    1st Commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    // Database
+    'database' => [
+        'engine' => 'mysql',
+        'host' => 'localhost:8889',
+        'database' => 'library',
+        'user' => 'root',
+        'password' => 'root'
+    ],
+
+    // Administrator auth
+    'admin' => ['admin', 'password']
+];
